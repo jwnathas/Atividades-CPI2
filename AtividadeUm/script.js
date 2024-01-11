01bd452a1ab54f1b1dde157e1170bca66da679da
Jonathas Silva <jonathasx57@gmail.com>
2024-01-11 20:37:16 -0300

    first commit

---
+++ AtividadeUm/script.js
@@ -0,0 +1,16 @@
+/* • Escreva uma função que retorne a soma entre dois números.
+
+ Exemplo: 
+
+ parametro1 = 1
+
+ parametro2 = 2
+
+ solucao(parametro1, parametro2) = 3*/
+
+function soma(num1, num2) {
+    let soma = num1 + num2;
+    return soma;
+};
+
+console.log(soma);
